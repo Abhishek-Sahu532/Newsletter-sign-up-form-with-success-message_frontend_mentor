ba0d40ad5e6f05f7144b0357f6173b04969fd13d
Abhishek Sahu <asahu532@gmail.com>
2023-08-11 17:36:07 +0530

    task completed

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
+let subscribeBtn = document.querySelector(".subscribeBtn");
 
-let subscribeBtn = document.querySelector('.subscribeBtn');
+subscribeBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  let emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-subscribeBtn.addEventListener('click', (event) => {
-    event.preventDefault();
-    let emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  let emailInput = document.getElementById("email").value;
+  // console.log(emailInput)
 
-    let emailInput = document.getElementById('email').value;
-    // console.log(emailInput)
+  if (emailFormat.test(emailInput)) {
+    document.querySelector(".index_main").style.display = "none";
+    document.querySelector(".successmain").style.display = "block";
 
-    if (emailFormat.test(emailInput)) {
-        document.querySelector('.index_main').style.display = 'none';
-        document.querySelector('.successmain').style.display = 'block'
-
-        let successMsg = document.querySelector('.successMsg p');
-        successMsg.innerHTML = `A confirmation email has been sent to ${emailInput}.
-         Please open it and click the button inside to confirm your subscription.`
-        return
-    } else {
-        let emailError = document.querySelector('#emailError');
-        emailError.style.display = 'block'
-    }
-})
-
+    let successMsg = document.querySelector(".successMsg p");
+    successMsg.innerHTML = `A confirmation email has been sent to ${emailInput}.
+         Please open it and click the button inside to confirm your subscription.`;
+    return;
+  } else {
+    let emailError = document.querySelector("#emailError");
+    emailError.style.display = "block";
+  }
+});
